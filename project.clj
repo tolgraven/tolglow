7f80ff5c6a103922f8d8c3f28a0861d77a35a5fe
Joen Tolgraven <joen.tolgraven@gmail.com>
2018-11-10 16:54:40 +0300

    allz

--- project.clj
+++ project.clj
@@ -2,17 +2,21 @@
  :description "tolgraven + afterglow "
  :url "http://example.com/FIXME"
  :license {:name "Secret" :url "none"}
- :dependencies [[org.clojure/clojure "1.8.0" #_"1.9.0"]
-                [quil "2.7.1"]
-                [mvxcvi/puget "1.0.2"]
-                [mvxcvi/whidbey "1.3.2"]
-                ;; [overtone "0.10.3"] ;in afterglow tho
-                [afterglow "0.2.4"]
-                [org.clojure/tools.namespace "0.2.7"]
-                [com.bhauman/rebel-readline "0.1.4"]]
-                ;; [afterglow "0.2.5-SNAPSHOT"]]
-;;  :main #_^:skip-aot tolglow.core #_nil
- :main tolglow.core
+ :dependencies
+ [[org.clojure/clojure "1.8.0" #_"1.9.0"]
+  [afterglow #_"0.2.5-SNAPSHOT" "0.2.4"] ;in checkouts, so keep out!! dep issues now there tho
+  [quil "2.7.1"]
+  [mvxcvi/puget "1.0.2"]
+  [mvxcvi/whidbey "1.3.2"]
+  ;; [overtone "0.10.3"] ;in afterglow tho
+  [org.clojure/tools.namespace "0.2.7"]
+  [ring-middleware-format "0.7.2"
+   :exclusions [ring/ring-jetty-adapter cheshire org.clojure/tools.reader
+                org.clojure/java.classpath org.clojure/core.memoize com.fasterxml.jackson.core/jackson-core]]
+  [com.bhauman/rebel-readline "0.1.4"]]
+ :main tolglow.core ;; #_^:skip-aot tolglow.core #_nil
+ :jvm-opts ;put in ext map and reference from that?
+ ["-Dapple.awt.UIElement=true"] ;background app / no dock icon
  #_:repl-options #_{:host "0.0.0.0" ; allow remote connections
                 :port 5000 ; consistent port
                 :init-ns tolglow.core}
@@ -29,9 +33,9 @@
                        :port 5000 ; consistent port
                        :welcome (println "tolglow loaded.")}
         :jvm-opts ["-XX:-OmitStackTraceInFastThrow"
-                   "-Dapple.awt.UIElement=true"
                    "-Dclojure.compiler.disable-locals-clearing=true"]
         #_:env #_{:dev "true"}}
   :uberjar {:aot :all
-            :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+            :jvm-opts ["-XX:-OmitStackTraceInFastThrow"
+                       "-Dclojure.compiler.direct-linking=true"]
             #_:env #_{:production "true"}}})
